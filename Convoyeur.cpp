f2d276d966aab069609d6c9ec04b4fa08faebd30
Abdoulllllll <watttabd@gmail.com>
2026-05-01 20:14:45 -0400

    Add files via upload

---
+++ Convoyeur.cpp
@@ -0,0 +1,217 @@
+#include "Convoyeur.h"
+
+// Initialisation de l'instance static du Convoyeur
+// Pour l'instant null
+Convoyeur *Convoyeur::instance = nullptr;
+
+// TODO : Compléter le constructeur
+Convoyeur::Convoyeur(byte motPin1, byte motPin2, byte manette, byte btnPin, byte aff_CLK, byte aff_DIN, byte aff_CS)
+  : _button(btnPin), _u8g2(U8G2_R0, aff_CLK, aff_DIN, aff_CS, U8X8_PIN_NONE)
+
+{
+  _button.attachClick(buttonClick, this);
+  _button.attachLongPressStart(longPress, this);
+
+
+  _moteur_pin_1 = motPin1;
+  _moteur_pin_2 = motPin2;
+  _manette = manette;
+  _btn_pin = btnPin;
+  _aff_CLK = aff_CLK;
+  _aff_DIN = aff_DIN;
+  _aff_CS = aff_CS;
+}
+
+
+void Convoyeur::buttonClick(void *context) {
+  Convoyeur *self = static_cast<Convoyeur *>(context);
+  self->_buttonPressed = true;
+}
+
+void Convoyeur::longPress(void *context) {
+  Convoyeur *self = static_cast<Convoyeur *>(context);
+  self->_longPress = true;
+}
+
+void Convoyeur::update() {
+  _currentTime = millis();
+  _button.tick();
+  switch (_state) {
+    case ConvState::ACTIF:
+      actifState();
+      break;
+    case ConvState::INACTIF:
+      inactifState();
+      break;
+    case ConvState::MANUEL:
+      manuelState();
+      break;
+    case ConvState::CONSTANT:
+      constantState();
+      break;
+  }
+}
+void Convoyeur::setMoteurVitesse(int vitesse) {
+
+  int min = 0;
+  int max = 255;
+  int minVitesse = 0;
+  int maxVitesse = 100;
+
+
+  _valeurM = map(abs(vitesse), minVitesse, maxVitesse, min, max);
+  if (vitesse > 0) {
+
+    analogWrite(_moteur_pin_1, _valeurM);
+    analogWrite(_moteur_pin_2, 0);
+
+
+  }
+
+  else if (vitesse < 0) {
+
+    analogWrite(_moteur_pin_2, _valeurM);
+    analogWrite(_moteur_pin_1, 0);
+
+  }
+
+  else {
+    analogWrite(_moteur_pin_2, 0);
+    analogWrite(_moteur_pin_1, 0);
+  }
+}
+
+int Convoyeur::getVitesseMoteur() {
+  return _valY;
+}
+
+void Convoyeur::setAffichage(int id_symbole) {
+
+  switch (id_symbole) {
+    case 0:
+      _u8g2.clearBuffer();
+      _u8g2.drawBitmap(0, 0, 1, 8, bitmapMoins);
+      _u8g2.sendBuffer();
+      break;
+    case 1:
+      _u8g2.clearBuffer();
+      _u8g2.drawBitmap(0, 0, 1, 8, bitmapPointExclamation);
+      _u8g2.sendBuffer();
+      break;
+    case 2:
+      _u8g2.clearBuffer();
+      _u8g2.drawBitmap(0, 0, 1, 8, bitmapHaut);
+      _u8g2.sendBuffer();
+      break;
+    case 3:
+      _u8g2.clearBuffer();
+      _u8g2.drawBitmap(0, 0, 1, 8, bitmapBas);
+      _u8g2.sendBuffer();
+      break;
+  }
+}
+
+void Convoyeur::begin() {
+  _u8g2.begin();
+  pinMode(_moteur_pin_1, OUTPUT);
+  pinMode(_moteur_pin_2, OUTPUT);
+  setMoteurVitesse(0);
+  setAffichage(0);
+}
+
+void Convoyeur::actifState() {
+  setMoteurVitesse(0);
+  setAffichage(1);
+  int min = -100;
+  int max = 100;
+  int minJoystick = 0;
+  int maxJoystick = 1023;
+
+  int joystickY = analogRead(_manette);
+  _valY = map(joystickY, minJoystick, maxJoystick, min, max);
+
+  if (abs(_valY) > _joystickDeadZone) {
+    _state = ConvState::MANUEL;
+  }
+  if (_buttonPressed) {
+    _buttonPressed = false;
+  }
+  if (_longPress) {
+    _state = ConvState::INACTIF;
+    _longPress = false;
+    return;
+  }
+}
+
+
+void Convoyeur::inactifState() {
+  setMoteurVitesse(0);
+  setAffichage(0);
+
+  if (_longPress) {
+    _state = ConvState::ACTIF;
+    _longPress = false;
+    return;
+  }
+}
+
+void Convoyeur::manuelState() {
+  int min = -100;
+  int max = 100;
+  int minJoystick = 0;
+  int maxJoystick = 1023;
+
+  int joystickY = analogRead(_manette);
+  int valY = map(joystickY, minJoystick, maxJoystick, min, max);
+
+  setMoteurVitesse(valY);
+  if (abs(valY) <= _joystickDeadZone) {
+    setMoteurVitesse(0);
+    _state = ConvState::ACTIF;
+    return;
+  } else if (valY < 0) {
+    setAffichage(3);
+  }
+
+  else {
+    setAffichage(2);
+  }
+
+  if (_longPress) {
+    _state = ConvState::INACTIF;
+    _longPress = false;
+    return;
+  }
+  if (_buttonPressed) {
+    _state = ConvState::CONSTANT;
+    _vitesse_constante = valY;
+    _buttonPressed = false;
+    return;
+  }
+}
+
+
+
+void Convoyeur::constantState() {
+  setMoteurVitesse(_vitesse_constante);
+
+  if (_vitesse_constante < 0) {
+    setAffichage(3);
+  }
+
+  else {
+    setAffichage(2);
+  }
+
+
+  if (_longPress) {
+    _state = ConvState::INACTIF;
+    _longPress = false;
+    return;
+  }
+  if (_buttonPressed) {
+    _state = ConvState::MANUEL;
+    _buttonPressed = false;
+    return;
+  }
+}
---
+++ Convoyeur.h
@@ -0,0 +1,111 @@
+#pragma once
+
+// Mettre l'enum avant les includes
+// Utiliser comme ceci ConvState::MARCHE, ConvState::ARRET...
+enum class ConvState { MANUEL,
+                       CONSTANT,
+                       ACTIF,
+                       INACTIF };
+
+#include <OneButton.h>
+#include <U8g2lib.h>
+
+
+class Convoyeur {
+public:
+
+  Convoyeur(byte motPin1, byte motPin2, byte manette, byte btnPin, byte aff_CLK, byte aff_DIN, byte aff_CS);
+
+  // Retourne vrai si le moteur est en fonction
+
+
+  // Met à jour les états du convoyeur
+  void update();
+  void begin();
+  int getVitesseMoteur();
+
+private:
+  unsigned long _currentTime = 0;
+
+  byte _moteur_pin_1 = 0;
+  byte _moteur_pin_2 = 0;
+  byte _manette = 0;
+  byte _btn_pin = 0;
+  byte _aff_CLK = 0;
+  byte _aff_DIN = 0;
+  byte _aff_CS = 0;
+
+
+  // À ajuster selon les besoins du joystick
+  // TODO : À rechercher sur Google
+  int _joystickDeadZone = 5;
+  int _vitesse_constante = 0;
+  int _valeurM = 0;
+  int _valY = 0;
+  OneButton _button;
+  bool _buttonPressed = false;
+  bool _longPress = false;
+
+  static Convoyeur *instance;
+  static void buttonClick(void *context);
+  static void longPress(void *context);
+
+  ConvState _state = ConvState::INACTIF;
+
+  U8G2_MAX7219_8X8_F_4W_SW_SPI _u8g2;
+
+  void manuelState();
+  void constantState();
+  void actifState();
+  void inactifState();
+
+  // Configure les broches du moteur
+  void setMoteurVitesse(int vitesse);
+
+  // Configure l'affichage du convoyeur
+  void setAffichage(int id_symbole);
+
+  const uint8_t bitmapPointExclamation[8] = {
+    B00011000,
+    B00011000,
+    B00011000,
+    B00011000,
+    B00011000,
+    B00000000,
+    B00011000,
+    B00000000
+  };
+
+  const uint8_t bitmapHaut[8] = {
+    B00011000,
+    B00111100,
+    B01111110,
+    B11111111,
+    B00011000,
+    B00011000,
+    B00011000,
+    B00000000
+  };
+
+  const uint8_t bitmapBas[8] = {
+    B00011000,
+    B00011000,
+    B00011000,
+    B11111111,
+    B01111110,
+    B00111100,
+    B00011000,
+    B00000000
+  };
+
+  const uint8_t bitmapMoins[8] = {
+    B00000000,
+    B00000000,
+    B00000000,
+    B11111111,
+    B11111111,
+    B00000000,
+    B00000000,
+    B00000000
+  };
+};
